80a9831487d7fe25e016e668abadebdc83417740
Raj Soni <rajksoni029@gmail.com>
2024-01-28 18:58:15 +0530

    JWT Token added

--- backend/user/views.py
+++ backend/user/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 
-# Create your views here.
+
+@api_view(['GET'])
+def get_routes(request):
+    routes = [
+        '/api/token',
+        '/api/token/refresh',
+    ]
+    
+    return Response(routes)
